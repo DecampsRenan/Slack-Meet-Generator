d2a1788a30f580652b4a1456c00ce6e1c0ecce2d
Renan <renan.decamps@gmail.com>
2021-02-23 18:29:46 +0100

    feat: add setup script
    - update readme with google setup

--- README.md
+++ README.md
@@ -1,9 +1,32 @@
 # Meet Generator for Slack
 
+This project uses your google account to create event in your calendar in order to get a meet link.
+
+It's a bit hacky, it's used in my company for more than 1 year at the time of writing this line !
+
+Basically, with that app you will be able to do `/meet` in any slack channel, and be able to get an auto generated Google Meet link !
 
 ## Installation
 
+First, duplicate the `.env.example` file and remove the .example suffix.
+You can already fill the `CALENDAR_EMAIL` with your google email account.
 
+### Configure a new Google Cloud Platform project
+
+1. Follow the step 1 here: https://developers.google.com/calendar/quickstart/nodejs. Make sure that you are logged with the google account you used to fill the `CALENDAR_EMAIL` env in the .env file.
+2. When asked to "Configure your OAuth client", select "Web Server". Fill the callback url with `http://localhost`
+3. Copy / paste the Client ID and Client Secret in the .env file (just duplicate the .env.example one).
+4. Run `yarn setup:credentials`. When asked for a google account, select the one used in the steps above.
+5. You will probably have a screen with a warning about an app not approved; you can continue with that (because it's **your** app, I will not steel any data :p )
+6. Accept all permissions
+7. Once redirected, copy the code in the url and paste it in your terminal
+(Auth Code)[./doc/auth-code.png]
+8. If the cli list some events and you can recognize them, then it's good ! You can copy / paste the jons logged after the CREDENTIALS keyword (be sure to not include trailings `"`)
+(Credentials)[./doc/credentials.png]
+9. Now your server should be able to contact your calendar and create event !
+
+### Configure Slack
+[WIP]
 
 
 ```bash
@@ -15,4 +38,8 @@ yarn install
 ```bash
 # development
 yarn start:dev
+
+# Prod
+yarn build
+yarn start
 ```
---
+++ scripts/getCredentials.js
@@ -0,0 +1,74 @@
+/* eslint-disable no-console */
+require('dotenv').config();
+
+const readline = require('readline');
+const { google } = require('googleapis');
+
+const SCOPES = [
+  'https://www.googleapis.com/auth/calendar.readonly',
+  'https://www.googleapis.com/auth/calendar.events',
+];
+
+function getAccessToken(oAuth2Client, callback) {
+  const authUrl = oAuth2Client.generateAuthUrl({
+    access_type: 'offline',
+    scope: SCOPES,
+  });
+  console.log('Authorize this app by visiting this url:', authUrl);
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  });
+  rl.question('Enter the code from that page here: ', (code) => {
+    rl.close();
+    oAuth2Client.getToken(code, (err, token) => {
+      if (err) {
+        console.error('Error retrieving access token', err);
+        return;
+      }
+      oAuth2Client.setCredentials(token);
+      // Store the token to disk for later program executions
+      console.log(`CREDENTIALS: "${JSON.stringify(token)}"`);
+      callback(oAuth2Client);
+    });
+  });
+}
+
+function authorize(callback) {
+  const oAuth2Client = new google.auth.OAuth2(
+    process.env.CLIENT_ID,
+    process.env.CLIENT_SECRET,
+    'http://localhost',
+  );
+
+  getAccessToken(oAuth2Client, callback);
+}
+
+function listEvents(auth) {
+  const calendar = google.calendar({ version: 'v3', auth });
+  calendar.events.list({
+    calendarId: process.env.CALENDAR_ID,
+    timeMin: (new Date()).toISOString(),
+    maxResults: 10,
+    singleEvents: true,
+    orderBy: 'startTime',
+  }, (err, res) => {
+    if (err) {
+      console.log(`The API returned an error: ${err}`);
+      return;
+    }
+
+    const events = res.data.items;
+    if (events.length) {
+      console.log('Upcoming 10 events:');
+      events.forEach((event) => {
+        const start = event.start.dateTime || event.start.date;
+        console.log(`${start} - ${event.summary}`);
+      });
+    } else {
+      console.log('No upcoming events found.');
+    }
+  });
+}
+
+authorize(listEvents);
